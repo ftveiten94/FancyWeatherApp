99c153c9f866a18400edfbc1b918e85a1605a0e0
Frode <ftveiten@gmail.com>
2025-04-15 00:48:18 +0200

    Første commit – la til weather app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Weatherapp from './Components/Weatherapp'
+import './Components/Weatherapp.css'
+const App = () => {
+  return (
+    <div>
+      <Weatherapp/>
+    </div>
+  )
+}
+
+export default App
